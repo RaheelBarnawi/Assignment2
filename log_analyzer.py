f4c69d85671efce043b8929e05427da1e82ef98a
RaheelBarnawi <raheel_barnawi@hotmail.com>
2017-03-30 23:53:59 -0400

    solved question1

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,29 +1,43 @@
-#!/usr/bin/env python
-
+# !/usr/bin/env python
 import sys
 from pyspark import SparkContext, SparkConf
 
-conf = SparkConf().setAppName("testFunction").setMaster("local")
+conf = SparkConf().setAppName("log_analyzer").setMaster("local")
 sc = SparkContext(conf=conf)
 
-question_number=sys.argv[1]
+question_number = sys.argv[1]
 input_text_file_1 = sys.argv[2]
 input_text_file_2 = sys.argv[3]
-if(question_number=="-q1"):
+
+if (question_number == "-q1"):
     print (" *Q1: line counts")
-elif (question_number=="-q2"):
+    counts_iliad = sc.textFile(input_text_file_1). \
+        count()
+    counts_odyssey = sc.textFile(input_text_file_2). \
+        count()
+    print '+ iliad:', counts_iliad
+    print '+ odyssey:', counts_odyssey
+
+
+elif (question_number == "-q2"):
     print (" Q2: sessions of user 'achille'")
-elif (question_number=="-q3"):
+elif (question_number == "-q3"):
     print ("* Q3: unique user names")
-elif (question_number=="-q4"):
+elif (question_number == "-q4"):
     print (" * Q4: sessions per user")
-elif (question_number=="-q5"):
+elif (question_number == "-q5"):
     print (" * Q5: number of errors")
-elif (question_number=="-q6"):
+elif (question_number == "-q6"):
     print (" * Q6: 5 most frequent error messages")
-elif (question_number=="-q7"):
+elif (question_number == "-q7"):
     print (" * Q7: users who started a session on both hosts, i.e., on exactly 2 hosts.")
-elif (question_number=="-q8"):
+elif (question_number == "-q8"):
     print (" * Q8: users who started a session on exactly one host, with host name.")
 elif (question_number == "-q9"):
     print (" ---------")
+
+
+
+
+
+
