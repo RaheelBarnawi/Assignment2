2c82ed48abfb40dc49614a3b02ef20c57a0c3e5c
RaheelBarnawi <raheel_barnawi@hotmail.com>
2017-03-30 22:12:44 -0400

    added if else statments

--- log_analyzer.py
+++ log_analyzer.py
@@ -6,13 +6,24 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName("testFunction").setMaster("local")
 sc = SparkContext(conf=conf)
 
-input_text_file = sys.argv[1]
-output_text_file = sys.argv[2]
-
-counts = sc.textFile(input_text_file).\
-         map(lambda line : line.split(" ")).
-
-
-
-
-result.saveAsTextFile(output_text_file)
+question_number=sys.argv[1]
+input_text_file_1 = sys.argv[2]
+input_text_file_2 = sys.argv[3]
+if(question_number=="-q1"):
+    print (" *Q1: line counts")
+elif (question_number=="-q2"):
+    print (" Q2: sessions of user 'achille'")
+elif (question_number=="-q3"):
+    print ("* Q3: unique user names")
+elif (question_number=="-q4"):
+    print (" * Q4: sessions per user")
+elif (question_number=="-q5"):
+    print (" * Q5: number of errors")
+elif (question_number=="-q6"):
+    print (" * Q6: 5 most frequent error messages")
+elif (question_number=="-q7"):
+    print (" * Q7: users who started a session on both hosts, i.e., on exactly 2 hosts.")
+elif (question_number=="-q8"):
+    print (" * Q8: users who started a session on exactly one host, with host name.")
+elif (question_number == "-q9"):
+    print (" ---------")
