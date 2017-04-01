4efcf66cb02b4472115a5adbb530ab546946dc37
RaheelBarnawi <raheel_barnawi@hotmail.com>
2017-04-01 14:47:21 -0400

    edited question 2 code

--- log_analyzer.py
+++ log_analyzer.py
@@ -4,6 +4,7 @@ from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setAppName("log_analyzer").setMaster("local")
 sc = SparkContext(conf=conf)
+if __name__=="__main __":
 
 question_number = sys.argv[1]
 input_text_file_1 = sys.argv[2]
@@ -24,15 +25,12 @@ elif (question_number == "-q2"):
     user_iliad = sc.textFile(input_text_file_1). \
         map(lambda x: x.replace(',', ' ').replace('.', ' ').lower()). \
         filter(lambda x: 'Starting session'.lower()). \
-        map(lambda x: x.split(" ")). \
-        filter(lambda x: 'achille' in x). \
-        count()
+        filter(lambda x: 'achille' in x).count()
+
     user_odyssey = sc.textFile(input_text_file_2). \
         map(lambda x: x.replace(',', ' ').replace('.', ' ').lower()). \
         filter(lambda x: 'Starting session'.lower()). \
-        map(lambda x: x.split(" ")). \
-        filter(lambda x: 'achille' in x). \
-        count()
+        filter(lambda x: 'achille' in x).count()
     print '+ iliad:', user_iliad
     print '+ odysse:',user_odyssey
 
@@ -49,6 +47,15 @@ elif (question_number == "-q3"):
     print '+ iliad:', unique_user_iliad
 elif (question_number == "-q4"):
     print (" * Q4: sessions per user")
+
+    unique_user_count_iliad = sc.textFile(input_text_file_1). \
+        map(lambda x: x.replace(',', ' ').replace('.', ' ').lower()). \
+        filter(lambda x: 'Starting session'.lower()). \
+        map(lambda x: x.split(" ")). \
+        filter(lambda x: (x[10])). \
+        map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + 1).\
+        distinct()
+    print '+ iliad:', unique_user_count_iliad
 elif (question_number == "-q5"):
     print (" * Q5: number of errors")
 elif (question_number == "-q6"):
